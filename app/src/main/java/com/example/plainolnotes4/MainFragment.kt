de56051242df9eac76303a4c77034c6d67646ccc
gouri-panda <gouripanda4@gmail.com>
2020-12-24 16:53:44 +0530

    added recyclerview  add item decoration and set fixed size to true

--- app/src/main/java/com/example/plainolnotes4/MainFragment.kt
+++ app/src/main/java/com/example/plainolnotes4/MainFragment.kt
@@ -6,24 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.example.plainolnotes4.databinding.MainFragmentBinding
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
-
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        recyclerview.apply {
+            setHasFixedSize(true)
+            val divider = DividerItemDecoration(context, LinearLayout.VERTICAL)
+            addItemDecoration(divider)
+        }
+
     }
+
 
 }
