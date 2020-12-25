25cf2d940639bb9a7c8435ff107173422a4e5f82
gouri-panda <gouripanda4@gmail.com>
2020-12-25 21:37:53 +0530

    added NoteDao.kt interface and NoteEntity.kt file

---
+++ app/src/main/java/com/example/plainolnotes4/data/NoteDao.kt
@@ -0,0 +1,22 @@
+package com.example.plainolnotes4.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface NoteDao {
+    @Insert
+    fun insertNote(noteEntity: NoteEntity)
+
+    @Delete
+    fun deleteNOte(noteEntity: NoteEntity)
+
+    @Query("SELECT * FROM notes ORDER BY date ASC")
+    fun getAllNote(): LiveData<List<NoteEntity>>
+
+    @Query("SELECT * FROM notes WHERE id = :id")
+    fun getAllByNotes(id: Int): NoteEntity?
+}
--- app/src/main/java/com/example/plainolnotes4/data/NoteEntity.kt
+++ app/src/main/java/com/example/plainolnotes4/data/NoteEntity.kt
@@ -1,9 +1,17 @@
 package com.example.plainolnotes4.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.plainolnotes4.NEW_NOTE_ID
 import java.util.*
 
-data class NoteEntity(val id: Int, val date: Date, val text: String) {
+@Entity(tableName = "notes")
+data class NoteEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val date: Date,
+    val text: String
+) {
     constructor() : this(NEW_NOTE_ID, Date(), "")
     constructor(date: Date, text: String) : this(NEW_NOTE_ID, date, text)
 }
