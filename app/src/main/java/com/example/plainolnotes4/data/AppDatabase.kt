af45860dc9196c6b7f634a3d0e04ef3074862ca5
gouri-panda <gouripanda4@gmail.com>
2020-12-26 15:22:53 +0530

    created database and typeconverter

---
+++ app/src/main/java/com/example/plainolnotes4/data/AppDatabase.kt
@@ -0,0 +1,29 @@
+package com.example.plainolnotes4.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+@TypeConverters(DateConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao?
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = INSTANCE ?: Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        "notepiano.db"
+                    ).build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
